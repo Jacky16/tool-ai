d0b5ac2c5a71ef86e0ce9b184836e55b7141a51f
Adria Martinez <adriamartinez94@gmail.com>
2023-02-01 19:25:56 +0100

    Install and init tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
