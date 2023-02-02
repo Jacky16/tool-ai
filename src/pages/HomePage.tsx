17f34114062fcb15cb2b235b8a39aba3548a92f9
Adria Martinez <adriamartinez94@gmail.com>
2023-02-02 15:38:00 +0100

    Change image of home page and improve styles of it

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,39 @@
-import { BsFillArrowDownCircleFill } from "react-icons/bs";
 import Container from "../components/Container/Container";
 import ToolsSection from "../components/ToolsSection/ToolsSection";
 
 const HomePage = () => {
   return (
     <div className="mt-8 flex flex-col gap-14">
-      <section className="flex flex-col items-center h-[50vh] relative">
+      <section className="flex flex-col items-center relative">
         <Container>
           <div className="flex flex-col">
-            <div className="md:flex md:flex-row items-start">
-              <p className="block font-extrabold text-5xl md:text-6xl w-56 md:w-96 text-tertiary">
-                Start using <span className="text-primary">AI</span> to improve
-                your
-                <span className="text-primary"> WORK</span>
-              </p>
+            <div className=" md:flex md:flex-row items-center justify-center gap-20">
+              <div className="flex flex-col items-center gap-8">
+                <p className="font-extrabold text-5xl md:text-6xl  text-center md:w-96 text-tertiary">
+                  Start using <span className="text-primary">AI</span> to
+                  improve your
+                  <span className="text-primary"> WORK</span>
+                </p>
+
+                <span className="text-tertiary text-xl md:text-2xl">
+                  Powered by{" "}
+                  <a
+                    className="underline"
+                    href="https://cohere.ai"
+                    target={"_blank"}
+                    rel="noreferrer"
+                  >
+                    Cohere
+                  </a>
+                </span>
+              </div>
               <img
-                height={"500px"}
-                width={"90%"}
-                className="rounded-3xl hidden md:block flex-1 max-w-xl"
-                src="/assets/images/AI-illustration.webp"
+                height={"300px"}
+                width={"300px"}
+                className="rounded-3xl hidden md:block max-w-xl bg-tertiary"
+                src="/assets/images/robot.webp"
                 alt="AI illustration of a man using a PC"
               />
-            </div>
-            <div className="flex flex-col items-center absolute bottom-0 right-[50%] translate-x-[50%]">
-              <BsFillArrowDownCircleFill
-                color="white"
-                size={48}
-                className="transition-transform animate-bounce"
-              />
-              <small className="text-tertiary text-lg">Scroll</small>
             </div>
           </div>
         </Container>
