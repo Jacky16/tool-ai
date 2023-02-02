93f9a0cbb20ec4e5762eb74960c259cf4a79f04f
Adria Martinez <adriamartinez94@gmail.com>
2023-02-02 11:55:44 +0100

    Implement header on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 const HomePage = lazy(() => import("./pages/HomePage"));
 
 function App() {
   return (
-    <div className="bg-background min-h-screen">
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <HomePage />
-            </Suspense>
-          }
-        />
+    <div className="bg-background min-h-screen py-6">
+      <div className="container mx-auto max-w-7xl">
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <HomePage />
+              </Suspense>
+            }
+          />
 
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </div>
     </div>
   );
 }
