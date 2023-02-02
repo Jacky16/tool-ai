1359c3d0fc6d2901a085a2acb65b081469e11180
Adria Martinez <adriamartinez94@gmail.com>
2023-02-02 15:44:00 +0100

    Put justify around in app

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const HomePage = lazy(() => import("./pages/HomePage"));
 
 function App() {
   return (
-    <div className="bg-background min-h-screen py-12 flex flex-col justify-between gap-12 min-h">
+    <div className="bg-background min-h-screen flex flex-col justify-around min-h py-8 gap-8">
       <Header />
       <Routes>
         <Route
