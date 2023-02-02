3d050e3fc8c3968629bbb74c525dc6b023254a5d
Adria Martinez <adriamartinez94@gmail.com>
2023-02-02 13:36:05 +0100

    Add Tool Card list component

---
+++ src/components/ToolCardList/ToolCardList.tsx
@@ -0,0 +1,20 @@
+import { Tool } from "../../types/types";
+import ToolCard from "../ToolCard/ToolCard";
+
+interface ToolCardListProps {
+  tools: Tool[];
+}
+
+const ToolCardList = ({ tools }: ToolCardListProps) => {
+  return (
+    <ul className="flex flex-col gap-12 md:flex-row md:grid grid-cols-3">
+      {tools.map((tool, index) => (
+        <li key={index}>
+          <ToolCard tool={tool} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ToolCardList;
