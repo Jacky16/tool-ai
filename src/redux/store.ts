eed92f1095e976985855f75a217b6258704f1ae8
Adria Martinez <adriamartinez94@gmail.com>
2023-02-04 20:09:00 +0100

    Set up uiReducer on store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import { toolsReducer } from "./toolsSlice/toolsSlice";
+import { uiReducer } from "./uiSlice/uiSlice";
 
 export const store = configureStore({
   reducer: {
     tools: toolsReducer,
+    ui: uiReducer,
   },
 });
 
