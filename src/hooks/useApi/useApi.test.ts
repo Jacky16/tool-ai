cb381dc29473d31b15071169b8359620c62dc2a1
Adria Martinez <adriamartinez94@gmail.com>
2023-02-05 16:51:22 +0100

    Adapt useAPi and his test

--- src/hooks/useApi/useApi.test.ts
+++ src/hooks/useApi/useApi.test.ts
@@ -2,7 +2,7 @@ import { renderHook } from "@testing-library/react";
 import { mockCheckEmailSpamResponse } from "../../mocks/mockToolsDataResponse";
 import RenderWrapper from "../../mocks/RenderWrapper";
 import { store } from "../../redux/store";
-import { loadSpamPercentageActionCreator } from "../../redux/toolsSlice/toolsSlice";
+import { loadSpamCheckerActionCreator } from "../../redux/toolsSlice/toolsSlice";
 import useApi from "./useApi";
 
 const dispatch = jest.spyOn(store, "dispatch");
@@ -11,9 +11,12 @@ describe("Given the checkEmailSpam", () => {
   describe("When is called with a 'This is a spam email' string", () => {
     test("Then the dispatch should be called with the confidence '0.9999999'", async () => {
       const emailContent = "This is a spam email";
-      const expectedAction = loadSpamPercentageActionCreator(
-        mockCheckEmailSpamResponse.classifications[0].confidence
-      );
+
+      const [emailClassification] = mockCheckEmailSpamResponse.classifications;
+      const expectedAction = loadSpamCheckerActionCreator({
+        confidence: emailClassification.confidence,
+        prediction: emailClassification.prediction,
+      });
 
       const {
         result: {
